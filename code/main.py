49b75684f0781418c84e0ecc54be0b886aa53630
jpsm-ulslo <jpsm.ulslo@gmail.com>
2025-08-29 16:36:55 +0100

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,5 +1,5 @@
 from _utils import iniciar_chrome, pesquisar_npdm_e_exportar
-from _test import ler_cdms_csv, processar_cdms_individualmente
+from _test import ler_cdms_excel, processar_cdms_individualmente
 import os
 
 def main():
@@ -7,22 +7,25 @@ def main():
     print("Abrindo Chrome...")
     driver = iniciar_chrome()
     
-    try:
-        # Etapa 1: Pesquisar NPDM e exportar resultados
-        arquivo_resultado = pesquisar_npdm_e_exportar(driver, npdm)
-        if arquivo_resultado:
-            print(f"Arquivo de resultados salvo: {arquivo_resultado}")
-        else:
-            print("Falha ao exportar resultados.")
-            return  # encerra se não exportou
-    finally:
-        driver.quit()
-        print("Chrome fechado.")
+    if False:
+        try:
+            # Etapa 1: Pesquisar NPDM e exportar resultados
+            arquivo_resultado = pesquisar_npdm_e_exportar(driver, npdm)
+            if arquivo_resultado:
+                print(f"Arquivo de resultados salvo: {arquivo_resultado}")
+            else:
+                print("Falha ao exportar resultados.")
+                return  # encerra se não exportou
+        finally:
+            driver.quit()
+            print("Chrome fechado.")
+    else:
+        arquivo_resultado = "Referencias 29-08-2025 14_04.xlsx"
 
     # Etapa 2: Processar arquivo exportado
     if arquivo_resultado and os.path.exists(arquivo_resultado):
         try:
-            cdms_info = ler_cdms_csv(arquivo_resultado)
+            cdms_info = ler_cdms_excel(arquivo_resultado)
             print(f"{len(cdms_info)} CDMs Comercializados encontrados.")
             processar_cdms_individualmente(cdms_info)
         except Exception as e:
